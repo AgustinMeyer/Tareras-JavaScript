53de14bb301d83ccb0c905ab58f4ee3c4957a552
agustinmeyer <agumeyer83@gmail.com>
2026-06-28 18:49:04 -0300

    Realice la tarea 3

---
+++ tarea/tarea3.js
@@ -0,0 +1,52 @@
+// Tarea 1
+
+for (let i = 1; i <=10 ; i++) {
+    console.log(i);
+    
+    
+}
+
+// Tarea 2
+
+let numero = prompt("Ingrese el numero de la tabla que quiera aprender:");
+
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${numero} * ${i} = ${numero*i}`);
+        
+    }
+
+// Tarea 3
+
+let suma = 0
+for (let i = 1; i <= 10; i++) {
+    suma = suma + i;
+    
+}
+console.log("La suma total es:" + suma);
+
+
+// While 1
+
+let numero = 1
+while (numero<= 10) {
+    console.log(numero);
+    numero ++;
+}
+
+// 2
+
+let cuentaRegrasiva = 10;
+while (cuentaRegrasiva>0) {
+    console.log(cuentaRegrasiva);
+    cuentaRegrasiva--;
+}
+console.log("💣 BOOMM!!");
+
+// 3
+
+let numerosPares= 2;
+
+while (numerosPares <= 20 ) {
+    console.log(numerosPares);
+    numerosPares= numerosPares + 2;
+}
