40ee034bee1a4084aafeebfd1df159b46f3c812a
agustinmeyer <agumeyer83@gmail.com>
2026-07-23 19:36:23 -0300

    Agregado de tarera-tiempo-asincrona.js y ejercicio 1 realizado

--- tarea/tarea-tiempo-asincronismo.js
+++ tarea/tarea-tiempo-asincronismo.js
@@ -1,3 +1,7 @@
+/*
+Ejercicio 1: "La Cuenta Regresiva de Despegue" (setInterval y DOM)
+Consigna: Crea una página web con un título grande que diga "10" y un botón que diga "Iniciar Cuenta Regresiva". Al hacer clic en el botón, debe comenzar un contador hacia atrás de 1 en 1 segundo hasta llegar a 0. Cuando llegue a 0, el texto del título debe cambiar a "¡Despegue! 🚀" y el contador debe detenerse automáticamente.
+*/
 const contadorElemento = document.querySelector("#contador");
 const btnIniciar = document.querySelector("#btnIniciar");
 let temporizador = null;
